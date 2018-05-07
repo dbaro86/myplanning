f68bda0bd49988e5f02b72bd1f964b4ba470b1c8
Duany Baró Menéndez <duany@generalsoftwareinc.com>
2018-05-07 14:40:52 -0600

    Adding admin Goal

--- myplanning/apps/goal/admin.py
+++ myplanning/apps/goal/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Goal
 # Register your models here.
+
+
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'start_date', 'compliance_date']
+
+admin.site.register(Goal, GoalAdmin)
