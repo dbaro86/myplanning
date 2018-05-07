de4d12f3fcb50532581cf2c46cfc54b167d6d435
Duany Baró Menéndez <duany@generalsoftwareinc.com>
2018-05-07 14:43:02 -0600

    Solving problems with models import

--- myplanning/apps/goal/apps.py
+++ myplanning/apps/goal/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class GoalsConfig(AppConfig):
-    name = 'apps.goals'
+class GoalConfig(AppConfig):
+    name = 'apps.goal'
