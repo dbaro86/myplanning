d8b0fc946070bf4d9b374ba21fde118e79b3a58c
Duany Baró Menéndez <duany@generalsoftwareinc.com>
2018-05-07 14:40:31 -0600

    Adding model Goal

--- myplanning/apps/goal/models.py
+++ myplanning/apps/goal/models.py
@@ -2,9 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext as _
 
 # Create your models here.
 
 
 class Goal(models.Model):
-    name = models.CharField('')
+    name = models.CharField(_('Name'), max_length=255)
+    description = models.TextField(_('Description'))
+    start_date = models.DateTimeField(_('Start Date'), null=True, blank=True)
+    compliance_date = models.DateTimeField(_('Compliance Date'), null=True, blank=True)
+
+    def __str__(self):
+        return self.name
